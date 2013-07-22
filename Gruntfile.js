7ecce39f61fec03d3b9e974694c003b43b34ed77
Vincent Coste <coste.vincent@gmail.com>
2013-07-22 17:30:58 +0200

    modified gruntfile for heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,38 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
+  // grunt.initConfig({
 
-    rig: {
-      build: {
-        src: 'src/webwriter.js',
-        dest: 'dist/webwriter.js'
-      }
-    },
+  //   rig: {
+  //     build: {
+  //       src: 'src/webwriter.js',
+  //       dest: 'dist/webwriter.js'
+  //     }
+  //   },
 
-    uglify: {
-      options: {
-        mangle: false
-      },
-      standard: {
-        files: {
-          'dist/webwriter.min.js': 'dist/webwriter.js'
-        }
-      }
-    },
+  //   uglify: {
+  //     options: {
+  //       mangle: false
+  //     },
+  //     standard: {
+  //       files: {
+  //         'dist/webwriter.min.js': 'dist/webwriter.js'
+  //       }
+  //     }
+  //   },
 
-    watch: {
-      scripts: {
-        files: ['src/*.js', 'src/**/*.js'],
-        tasks: 'default'
-      }
-    }
+  //   watch: {
+  //     scripts: {
+  //       files: ['src/*.js', 'src/**/*.js'],
+  //       tasks: 'default'
+  //     }
+  //   }
 
-  });
+  // });
 
-  grunt.loadNpmTasks('grunt-rigger');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  // grunt.loadNpmTasks('grunt-rigger');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['rig', 'uglify']);
+  // grunt.registerTask('default', ['rig', 'uglify']);
 
 };
